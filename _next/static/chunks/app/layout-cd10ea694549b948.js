cfacde1216be9132ba4508d195e00893918997e8
hxtxrchq <calonsoparedes1@gmail.com>
2025-06-25 16:56:21 -0500

    Add files via upload

---
+++ _next/static/chunks/app/layout-cd10ea694549b948.js
@@ -0,0 +1,1 @@
+(self.webpackChunk_N_E=self.webpackChunk_N_E||[]).push([[177],{5743:(e,s,l)=>{Promise.resolve().then(l.t.bind(l,7459,23)),Promise.resolve().then(l.t.bind(l,9324,23))},7459:e=>{e.exports={style:{fontFamily:"'Inter', 'Inter Fallback'",fontStyle:"normal"},className:"__className_e8ce0c"}},9324:()=>{}},e=>{var s=s=>e(e.s=s);e.O(0,[385,441,684,358],()=>s(5743)),_N_E=e.O()}]);
